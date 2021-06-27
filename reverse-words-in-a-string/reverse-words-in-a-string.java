a9929589bb39818ae9627cdabccacb86c3c670f6
Charishma Boddu <charishma0249@gmail.com>
2021-06-27 22:05:01 +0530

    Time: 3 ms (90.65%), Space: 38.7 MB (92.21%) - LeetHub

---
+++ reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    StringBuilder res = new StringBuilder();
+    public String reverseWords(String s) {
+        
+        LinkedList<String> ll = new LinkedList<>();
+        
+        int len = s.length();
+        
+        for(int i=0;i<len;i++)
+        {
+            
+            char c = s.charAt(i);
+            if(c==' ')
+            {
+                continue;
+            }
+            
+            int j=i;
+            while(c!=' ' && i<len-1)
+            {
+                i++;
+                c=s.charAt(i);
+            }
+            
+            if(i==len-1)
+                ll.add(s.substring(j,i+1));
+            else
+                ll.add(s.substring(j,i));
+        }
+        
+        for(int i=ll.size()-1;i>=0;i--)
+        {
+            res.append(ll.get(i));
+            
+            if(i!=0)
+                res.append(" ");
+        }
+        //return rec(ll, 0, ll.size()).toString();
+        
+        return res.toString();
+            
+    }
+}
