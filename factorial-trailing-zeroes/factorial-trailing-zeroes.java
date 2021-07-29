55b3e5de1f7d06dd41c0f7cdf12df6f5c33c17b4
Charishma Boddu <charishma0249@gmail.com>
2021-07-29 21:38:32 +0530

    Time: 0 ms (100.00%), Space: 35.9 MB (50.60%) - LeetHub

--- factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -2,25 +2,16 @@ import java.math.*;
 class Solution {
     public int trailingZeroes(int n) {
         
-        int count1=0, count2=0;
-        
-        for(int i=n ; i>1;i--)
+        int count1=0;
+            
+        while(n>0)
         {
-            int temp = i;
-            while(temp>1 && temp%5==0)
-            {
-                count1++;
-                temp=temp/5;
-            }
-            temp=i;
-            while(temp>1 && temp%2==0)
-            {
-                count2++;
-                temp=temp/2;
-            }
+           n=n/5;
+           count1+=n;
         }
+            
         
-        return Math.min(count1, count2);
+        return count1;
     }
     
 
