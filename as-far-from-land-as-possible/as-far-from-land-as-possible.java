9997c2553edc4d9418e4d70a6de8cc32c5caf981
Charishma Boddu <charishma0249@gmail.com>
2021-08-03 21:51:06 -0400

    Time: 345 ms (8.52%), Space: 40.7 MB (26.34%) - LeetHub

--- as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -1,55 +1,63 @@
 class Solution {
     
-    int[] row;
-    int[] col;
-    int k=-1, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    int rlen, collen,k,l;
     public int maxDistance(int[][] grid) {
         
-        int len = grid.length;
-        int size =len*len;
+        rlen = grid.length;
+        collen = grid[0].length;
         
-        row = new int[size];
-        col = new int[size];
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
         
-        for(int i=0;i<len;i++)
+        for(int i=0; i<rlen; i++)
         {
-            for(int j=0;j<len;j++)
+            for(int j=0; j<collen; j++)
             {
+                
                 if(grid[i][j]==1)
-                {
-                    k++;
-                    row[k]=i;
-                    col[k]=j;
-                }
-            }
+                    grid[i][j]=0;
+                else
+                    grid[i][j]=min;
+            }   
         }
         
-        if(k==-1)
-            return -1;
-        
-        for(int i=0;i<len;i++)
+        for(int i=0; i<rlen; i++)
         {
-            for(int j=0;j<len;j++)
+            for(int j=0; j<collen; j++)
             {
                 if(grid[i][j]==0)
                 {
-                    min = Integer.MAX_VALUE;
-                    for(int l=0;l<=k;l++)
-                    {
-                        int sum =Math.abs(i-row[l])+Math.abs(j-col[l]);
-                        
-                        if(sum<min)
-                            min=sum;
-                    }
-                    
-                    if(min>max)
-                        max=min;
+                    k=i;
+                    l=j;
+                    rec(grid, i, j, grid[i][j]);
                 }
             }
         }
         
-        return (max == Integer.MIN_VALUE)? -1 : max;
+        for(int i=0; i<rlen; i++)
+        {
+            for(int j=0; j<collen; j++)
+            {
+                max = Math.max(max, grid[i][j]);
+            }
+        }
+        
+        return (max==Integer.MAX_VALUE || max==0)? -1 : max;
+    }
+    
+    public void rec(int[][] grid, int i, int j, int val)
+    {
+        if(i<0 || j<0 || i>=rlen || j>=collen || ((k!=i || l!=j) && val+1>=grid[i][j]))
+            return ;
+        
+        if(val+1<grid[i][j])
+            grid[i][j] = val+1;
+        
+        rec(grid, i+1, j, grid[i][j]);
+        rec(grid, i, j-1, grid[i][j]);
+        rec(grid, i-1, j, grid[i][j]);
+        rec(grid, i, j+1, grid[i][j]);
         
     }
+    
 
 }
