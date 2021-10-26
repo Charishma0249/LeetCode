8da06cb9be3e533f5479efab707c16ac45a347bf
Charishma Boddu <charishma0249@gmail.com>
2021-10-26 00:47:34 -0700

    Time: 2825 ms (5.10%), Space: 38.8 MB (56.88%) - LeetHub

---
+++ count-number-of-teams/count-number-of-teams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        
+        int count =0, len = rating.length;
+        for(int i=0; i<len; i++) {
+            for(int j=i+1; j<len; j++) {
+                for(int k=j+1; k<len; k++) {
+                    
+                    if(rating[i]>rating[j] && rating[j] > rating[k] || rating[i]<rating[j] && rating[j]<rating[k])
+                        count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
