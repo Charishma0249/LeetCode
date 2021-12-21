0e2b0da7049f392947e2402b689a1156f986cb6c
Charishma Boddu <charishma0249@gmail.com>
2021-12-21 13:37:05 -0700

    Time: 7 ms (54.16%), Space: 40.8 MB (36.99%) - LeetHub

--- island-perimeter/island-perimeter.java
+++ island-perimeter/island-perimeter.java
@@ -1,54 +1,44 @@
 class Solution {
     
-    int rlen, collen, count =0, blocks=0;
+    int perimeter = 0;
     public int islandPerimeter(int[][] grid) {
         
-        rlen = grid.length;
-        collen = grid[0].length;
+        int rlen = grid.length, collen = grid[0].length;
         
         for(int i=0; i<rlen; i++) {
             for(int j=0; j<collen; j++) {
-                
                 if(grid[i][j]==1) {
-                    //count-=2;
-                    rec(i, j, grid);
+                  //  perimeter=2;
+                    findPerimeter(i, j, rlen, collen, grid);
+                    //return perimeter;
                     break;
                 }
-                        
             }
         }
-        
-        return blocks*4-count;
+        return perimeter;
     }
     
-    public void rec(int i, int j, int[][] grid) {
+    public void findPerimeter(int i, int j, int rlen, int collen, int[][] grid) {
         
+        if(i<0 || i>=rlen || j<0 || j>=collen || grid[i][j]==0)
+            return ;
         
-        if(i>=rlen || j>=collen || i<0 || j<0 || grid[i][j]!=1)
-            return;
-        
-        
-        if(grid[i][j]==1) {
-            
-                //count+=2;
-            grid[i][j]=-1;
-            blocks++;
-            
-            if(j+1<=collen-1 && grid[i][j+1]!=0)
-                count++;
-            if(i+1<=rlen-1 && grid[i+1][j]!=0)
-                count++;
-            if(i-1>=0 && grid[i-1][j]!=0)
-                count++;
-            if(j-1>=0 && grid[i][j-1]!=0)
-                count++;
+        if(grid[i][j]==-1) {
+            perimeter--;
+            return ;
             
         }
+            
+        grid[i][j]=-1;
+        perimeter+=4;
         
-        rec(i+1, j, grid);
-        rec(i, j-1, grid);
-        rec(i-1, j, grid);
-        rec(i, j+1, grid);
+        if(perimeter>4)
+            perimeter--;
+       // perimeter-=2;
         
+        findPerimeter(i+1, j, rlen, collen, grid);
+        findPerimeter(i, j-1, rlen, collen, grid);
+        findPerimeter(i-1, j, rlen, collen, grid);
+        findPerimeter(i, j+1, rlen, collen, grid);
     }
 }
