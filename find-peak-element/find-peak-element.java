c898238dc4f770ee0e5566d8561c73bcc956cb12
Charishma Boddu <charishma0249@gmail.com>
2021-12-22 20:36:23 -0700

    Time: 0 ms (100.00%), Space: 38.3 MB (92.26%) - LeetHub

--- find-peak-element/find-peak-element.java
+++ find-peak-element/find-peak-element.java
@@ -1,25 +1,38 @@
 class Solution {
+    int res;
     public int findPeakElement(int[] nums) {
         
         int len = nums.length;
-        
         if(len==1)
             return 0;
         
-       int n = nums[0]; 
-       for(int i=1; i+1<len; i++) {
-           
-           if(nums[i]>n && nums[i]>nums[i+1])
-               return i;
-           n = nums[i];
-       }
-        
-       if(nums[len-1]>n) 
-           return len-1;
-        if(n>nums[len-1])
-            return 0;
-        return 0;
+        binSearch(nums, 0, len/2, len-1);
+        return res;
     }
     
-    
+    public void binSearch(int[] nums, int low, int mid, int high) {
+        
+        //System.out.println(mid);
+        if(mid>0 && mid<nums.length-1 && nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) {
+            res = mid;
+            return;
+        }
+        
+        else if(mid==0 && nums[mid]>nums[mid+1]) {
+            res = mid;
+            return;
+        }
+        
+        else if(mid==nums.length-1 && nums[mid]>nums[mid-1]) {
+            res = mid;
+            return;
+        }
+            
+        
+        if(low==high || low+1==high)
+            return ;
+        
+        binSearch(nums, low, (low+mid)/2, mid);
+        binSearch(nums, mid, (high+mid+1)/2, high);
+    }
 }
