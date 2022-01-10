75cb119cad829cf9dbeead0adb32b6ce142f25a9
Charishma Boddu <charishma0249@gmail.com>
2022-01-10 12:10:48 -0700

    Time: 0 ms (100.00%), Space: 36 MB (36.79%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean divisorGame(int n) {
         
         int ac=0, bc=0;
-        int[] dp = new int[n+1];
-        dp[1] = 0;
-        for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1]+1;
-        }
+        // int[] dp = new int[n+1];
+        // dp[1] = 0;
+        // for(int i=2; i<=n; i++) {
+        //     dp[i] = dp[i-1]+1;
+        // }
         
-        if(dp[n]%2==0)
-            return false;
-        return true;
+        if(n%2==0)
+            return true;
+        return false;
     }
 }
