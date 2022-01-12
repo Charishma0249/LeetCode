e06a8de89c317f1a2a65f59afaa897143f113f09
Charishma Boddu <charishma0249@gmail.com>
2022-01-12 10:24:06 -0700

    Time: 2 ms (29.03%), Space: 38.5 MB (20.86%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -2,30 +2,15 @@ class Solution {
     public List<Integer> getRow(int rowIndex) {
         
         List<Integer> al1 = new ArrayList<>();
-        al1.add(1);
-        List<Integer> al2 = new ArrayList<>();
-        al2.add(1);
-        al2.add(1);
-        //al2=al1;
-        if(rowIndex==0)
-            return al1;
-        else if(rowIndex==1)
-            return al2;
-        
-        
-        for(int i=2; i<=rowIndex; i++) {
-            al1 = new ArrayList<>();
-            for(int j=0; j<=i; j++) {
-              //  System.out.println(j);
-                if(j==0 || j==i)
-                    al1.add(1);
-                else
-                    al1.add(al2.get(j-1)+al2.get(j));
+        //al1.add(1);
+        for(int i=0; i<=rowIndex; i++)
+            al1.add(1);
+        for(int i=1; i<rowIndex; i++) {
+            for(int j=i; j>0; j--) {
+                al1.set(j, al1.get(j)+al1.get(j-1));
             }
-            //al2 =new ArrayList<>(al1);
-            al2 = al1;
         }
         
-        return al2;
+        return al1;
     }
 }
