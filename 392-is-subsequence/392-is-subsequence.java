3ac4ad63932b9187419b5c7e93931c0ed766a584
Charishma Boddu <charishma0249@gmail.com>
2022-01-12 22:48:35 -0700

    Time: 0 ms (100.00%), Space: 38.9 MB (22.50%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,40 +1,13 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
-        HashMap<Character, PriorityQueue<Integer>> hm = new HashMap<>();
-        
-        for(int i=0; i<t.length(); i++) {
-            
-            char c = t.charAt(i);
-            //HashSet<Integer> hs = new HashSet<>();
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            if(hm.containsKey(c))
-                pq = hm.get(c);
-            
-            pq.add(i);
-            hm.put(c, pq);
-        }
-        
-        int indx = -1;
+        int index = 0;
         for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
-           // System.out.println(c);
-            if(!hm.containsKey(c))
+            int lastIndx = t.indexOf(c, index);
+            if(lastIndx == -1)
                 return false;
-            else {
-                PriorityQueue<Integer> pq = hm.get(c);
-                int temp=indx;
-                while(!pq.isEmpty()) {
-                    int j = pq.poll();
-                    if(j>indx) {
-                      //  System.out.println(j);
-                        indx=j;
-                        break;
-                    }
-                }
-                if(pq.isEmpty() && indx==temp)
-                    return false;
-            }
+            index = lastIndx+1;
         }
         
         return true;
