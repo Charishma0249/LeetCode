1585a4d42a35e432df3fa0cbe1dec5818a42f7e6
Charishma Boddu <charishma0249@gmail.com>
2022-01-16 14:21:27 -0700

    Time: 3 ms (38.35%), Space: 38.7 MB (28.40%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stk = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c==')' || c==']' || c=='}') {
+                if(stk.isEmpty())
+                    return false;
+                char temp = stk.pop();
+                if((c==')' && temp!='(') || (c==']' && temp!='[') || (c=='}' && temp!='{'))
+                    return false;
+            }
+            else
+                stk.push(c);
+        }
+        
+        if(!stk.isEmpty())
+            return false;
+        return true;
+    }
+}
