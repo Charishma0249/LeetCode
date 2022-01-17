008dab78d74b76b821b3ff99807ccd86f9242545
Charishma Boddu <charishma0249@gmail.com>
2022-01-17 15:25:54 -0700

    Time: 3 ms (55.15%), Space: 52.5 MB (8.35%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minMoves(int[] nums) {
         
-        int min = Integer.MAX_VALUE;
-        int moves = 0, sum=0;
+        int min = nums[0];
+        int moves = 0, sum=nums[0];
         
-        for(int i=0; i<nums.length; i++) {
+        for(int i=1; i<nums.length; i++) {
             if(nums[i]<min)
                 min = nums[i];
             sum+=nums[i];
