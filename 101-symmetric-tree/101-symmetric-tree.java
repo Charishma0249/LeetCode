ea38a12be4725562c77c7f2c3bf5b3d6f9a01a78
Charishma Boddu <charishma0249@gmail.com>
2022-02-02 23:45:06 -0700

    Time: 2 ms (10.77%), Space: 42.4 MB (5.15%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -14,22 +14,25 @@
  * }
  */
 class Solution {
+    int i=0;
     public boolean isSymmetric(TreeNode root) {
         ArrayList<Integer> all = new ArrayList<>();
         ArrayList<Integer> alr = new ArrayList<>();
         dfsL(root.left, all);
-        dfsR(root.right, alr);
         
-        if(all.size()!=alr.size())
-            return false;
         
-        for(int i=0; i<all.size(); i++) {
-            // System.out.println(all.get(i) +" "+alr.get(i));
-            if(all.get(i)!=alr.get(i))
-                return false;
-        }
+        return dfsR(root.right, all);
         
-        return true;
+//         if(all.size()!=alr.size())
+//             return false;
+        
+//         for(int i=0; i<all.size(); i++) {
+//             // System.out.println(all.get(i) +" "+alr.get(i));
+//             if(all.get(i)!=alr.get(i))
+//                 return false;
+//         }
+        
+//         return true;
     }
     
     public void dfsL(TreeNode node, ArrayList<Integer> all) {
@@ -44,15 +47,31 @@ class Solution {
         dfsL(node.right, all);
     }
     
-    public void dfsR(TreeNode node, ArrayList<Integer> alr) {
+    public boolean dfsR(TreeNode node, ArrayList<Integer> all) {
         
+        boolean res1=true, res2=true;
         if(node==null) {
-            alr.add(null);
-            return ;
+            if(all.get(i)!=null) {
+                // System.out.println(all.get(i));
+                return false;
+            }
+            return true;
         }
         // System.out.println(node.val);
-        alr.add(node.val);
-        dfsR(node.right, alr);
-        dfsR(node.left, alr);
+        if(all.get(i)==null) {
+            // System.out.println("hey -- "+i);
+            return false;
+        }
+        if(all.get(i)!=node.val) {
+            // System.out.println(all.get(i)+" " +node.val);
+            return false;
+        }
+        // System.out.println("Hi -- "+all.get(i)+" "+node.val);
+        i++;
+        res1 = dfsR(node.right, all);
+        i++;
+        res2 = dfsR(node.left, all);
+        
+        return res1&&res2;
     }
 }
