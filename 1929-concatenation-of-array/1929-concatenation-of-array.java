18e4f15e7224d4fdd796e97c905554adbc0255a4
Charishma Boddu <charishma0249@gmail.com>
2022-02-03 23:39:42 -0700

    Time: 1 ms (78.43%), Space: 49.8 MB (7.92%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -4,7 +4,8 @@ class Solution {
         int len = nums.length;
         int[] res = new int[2*len];
         
-        for(int j=0, k=-1; j<len; j++) {
+        int k=-1;
+        for(int j=0; j<len; j++) {
             
             res[++k] = nums[j];
             res[len+k] = nums[j];
