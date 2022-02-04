3befddf455abf2317dc07595015d905637844a2a
Charishma Boddu <charishma0249@gmail.com>
2022-02-04 00:07:05 -0700

    Time: 1 ms (69.64%), Space: 40 MB (5.59%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int countPoints(String rings) {
+        
+        HashMap<Integer, int[]> hm = new HashMap<>();
+        int res=0;
+        
+        for(int i=0; i<rings.length(); ) {
+            int r = rings.charAt(i+1)-'0';
+            char c = rings.charAt(i);
+            
+            int[] colors = new int[3];
+            if(hm.containsKey(r))
+                colors = hm.get(r);
+            if(c=='R')
+                colors[0]++;
+            else if(c=='G')
+                colors[1]++;
+            else
+                colors[2]++;
+            
+            hm.put(r, colors);
+            i=i+2;
+        }
+        
+        for(Map.Entry<Integer, int[]> entry : hm.entrySet()) {
+            int[] colors = entry.getValue();
+            int count =0;
+            // System.out.println(entry.getKey());
+            for(int i=0; i<3; i++) {
+                // System.out.println(colors[i]);
+                if(colors[i]>=1)
+                    count++;
+            }
+            if(count==3)
+                res++;
+        }
+        
+        return res;
+    }
+}
