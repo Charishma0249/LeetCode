bf8c0f10d519c8b8f81785fb8ea6dd34fcf3b228
Charishma Boddu <charishma0249@gmail.com>
2022-02-05 16:05:37 -0700

    Time: 5 ms (57.23%), Space: 41.1 MB (15.59%) - LeetHub

---
+++ 824-goat-latin/824-goat-latin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        HashSet<Character> hs = new HashSet<>(Arrays.asList('a','e','o','u','i','A','E','O','U','I'));
+        
+        String[] str = sentence.split("\\s+");
+        StringBuilder res = new StringBuilder();
+        
+        for(int i=0; i<str.length; i++){
+            StringBuilder sb = new StringBuilder(str[i]);
+            char c = sb.charAt(0);
+            if(!hs.contains(c)) {
+                sb = new StringBuilder(sb.substring(1));
+                sb.append(c);
+            }
+            sb.append("ma");
+            for(int j=0; j<i+1;j++)
+                sb.append('a');
+            
+            res.append(sb);
+            res.append(" ");
+        }
+        return res.substring(0, res.length()-1);
+    }
+}
