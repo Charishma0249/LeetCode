70f42d11de7a5e5fabad7d0ef0d42da21872a435
Charishma Boddu <charishma0249@gmail.com>
2022-02-04 19:04:36 -0700

    Time: 66 ms (70.19%), Space: 62.7 MB (47.02%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,59 +1,26 @@
 class Solution {
-    List<String> res;
     public List<String> removeSubfolders(String[] folder) {
         
-        res = new ArrayList<>();
-        Trie root = new Trie();
+        List<String> res = new ArrayList<>();
+        Arrays.sort(folder);
+        res.add(folder[0]);
         
-        for(int i=0; i<folder.length; i++) {
-            // String s = folder[i];
-            String[] temp = folder[i].split("/");
-            Trie trie = root;
+        for(int i=1; i<folder.length; i++) {
+            String s = folder[i];
             
-            for(int j=0; j<temp.length; j++) {
-                if(temp[j].equals(""))
-                    continue;
-                // System.out.println(temp[j]);
-                Trie next = new Trie();
-                if(trie.alp.containsKey(temp[j]))
-                    next = trie.alp.get(temp[j]);
-                
-                trie.alp.put(temp[j], next);
-                trie = next;
-            }
-            trie.eof=1;
-        }
-        
-        removeSubfolder(root, new StringBuilder());
-        return res;
-    }
-    
-    public void removeSubfolder(Trie trie, StringBuilder sb) {
-        
-        for(Map.Entry<String, Trie> entry : trie.alp.entrySet()) {
-            
-            String s = entry.getKey();
-            Trie next = entry.getValue();
-            sb.append("/");
-            sb.append(s);
-            if(next.eof==1) {
-                res.add(sb.toString());
-            }
+            String prev = res.get(res.size()-1);
+            if(s.indexOf(prev)==-1)
+                res.add(s);
             else {
-                removeSubfolder(next, sb);
+                int n = s.indexOf(prev);
+                if(n!=0)
+                    res.add(s);
+                if(s.charAt(n+prev.length())!='/')
+                    res.add(s);
             }
-            int len = sb.length();
-            for(int i=0; i<=s.length(); i++) 
-                sb.deleteCharAt(len-i-1);
-            
         }
+        
+        return res;
+        
     }
-}
-
-class Trie{
-
-    HashMap<String, Trie> alp = new HashMap<>();
-    int eof;
-    
-    Trie(){}
 }
