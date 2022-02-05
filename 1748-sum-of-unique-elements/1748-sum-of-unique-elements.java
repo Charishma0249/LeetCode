7243a294b30a152ef6f5470ef9fe68e0fbb048bd
Charishma Boddu <charishma0249@gmail.com>
2022-02-05 15:01:16 -0700

    Time: 1 ms (78.46%), Space: 42.7 MB (5.45%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,18 +1,19 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
         int res =0;
-        Arrays.sort(nums);
-        if(nums.length==1)
-            return nums[0];
-        for(int i=0; i<nums.length; i++){
-            if(i==0 && nums[i]!=nums[i+1])
-                res+=nums[i];
-            else if(i==nums.length-1 && nums[i]!=nums[i-1])
-                res+=nums[i];
-            else if(i!=0 && i!= nums.length-1 && nums[i]!=nums[i-1] && nums[i]!=nums[i+1])
-                res+=nums[i];
+//         Arrays.sort(nums);
+//         if(nums.length==1)
+//             return nums[0];
+//         for(int i=0; i<nums.length; i++){
+//             if(i==0 && nums[i]!=nums[i+1])
+//                 res+=nums[i];
+//             else if(i==nums.length-1 && nums[i]!=nums[i-1])
+//                 res+=nums[i];
+//             else if(i!=0 && i!= nums.length-1 && nums[i]!=nums[i-1] && nums[i]!=nums[i+1])
+//                 res+=nums[i];
             
-        }
+//         }
+        /* -------2nd way------- */
 //         HashMap<Integer, Integer> hm = new HashMap<>();
 //         for(int i=0; i<nums.length;i++) {
 //             int count =0;
@@ -26,6 +27,13 @@ class Solution {
 //                 res += entry.getKey();
 //         }
         
+        int[] n = new int[101];
+        for(int i=0; i<nums.length; i++)
+            n[nums[i]]++;
+        for(int i=0; i<100; i++) {
+            if(n[i+1]==1)
+                res+=i+1;
+        }
         return res;
     }
 }
