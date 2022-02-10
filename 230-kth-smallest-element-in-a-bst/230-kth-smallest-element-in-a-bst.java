3981a343ed6a8c56b403c1b724ef14e87071524f
Charishma Boddu <charishma0249@gmail.com>
2022-02-10 00:38:53 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (59.96%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -14,17 +14,16 @@
  * }
  */
 class Solution {
-    List<Integer> al;
     int i=0, res=-1;
     public int kthSmallest(TreeNode root, int k) {
-        // al = new ArrayList<>();
         inorder(root, k);
         return res;
-        // return al.get(k-1);
     }
     
     public void inorder(TreeNode node, int k){
         
+        if(res!=-1)
+            return ;
         if(node.left==null && node.right==null) {
             i++;
             if(i==k)
@@ -34,9 +33,7 @@ class Solution {
         
         if(node.left!=null)
             inorder(node.left, k);
-        if(res!=-1)
-            return ;
-        // al.add(node.val);
+        
         i++;
         if(i==k) {
             res = node.val;
