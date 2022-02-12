d853f28715beeee3f933a4ee4dc41367dde79181
Charishma Boddu <charishma0249@gmail.com>
2022-02-12 12:25:03 -0700

    Time: 2 ms (96.25%), Space: 41.9 MB (40.30%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    StringBuilder res = new StringBuilder();
+    public String longestNiceSubstring(String s) {
+        
+        findSubstring(s, 0, s.length(), new StringBuilder());
+        return res.toString();
+    }
+    
+    public void findSubstring(String s, int startIdx, int endIdx, StringBuilder sb){
+        
+        
+        if(startIdx+1== endIdx)
+            return ;
+        HashSet<Character> hs = new HashSet<>();
+        for(int i=startIdx; i<endIdx; i++){
+            char c = s.charAt(i);
+            hs.add(c);
+        }
+        int i=0;
+        for(i = startIdx; i<endIdx; i++){
+            char c = s.charAt(i);
+            if(hs.contains(Character.toUpperCase(c)) && hs.contains(Character.toLowerCase(c))) {
+               sb.append(c); 
+               continue;
+            }
+               
+            // if(res.length()<sb.length()){
+            //     res = sb;
+            // }
+            findSubstring(s, i+1, endIdx, new StringBuilder());
+            findSubstring(s, startIdx, i, new StringBuilder());
+            break;
+        }
+        if(i==endIdx && res.length()<=sb.length()){
+            // System.out.println()
+                res = sb;
+            }
+    }
+}
