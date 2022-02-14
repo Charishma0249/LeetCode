3c587fdc03a03f034f20ae5fd617641a5d1b97c4
Charishma Boddu <charishma0249@gmail.com>
2022-02-14 15:56:25 -0700

    Time: 7 ms (81.07%), Space: 54.5 MB (25.45%) - LeetHub

---
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.java
@@ -0,0 +1,23 @@
+public class Codec {
+    HashMap<String, List<String>> hm = new HashMap<>();
+    
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<strs.size(); i++){
+            sb.append(strs.get(i));
+        }
+        
+        hm.put(sb.toString(), strs);
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        return hm.get(s);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
