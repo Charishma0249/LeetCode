27404520672236ce62a39a1482fe3fe5ab586235
Charishma Boddu <charishma0249@gmail.com>
2022-02-15 23:20:16 -0700

    Time: 4 ms (77.98%), Space: 61.8 MB (21.74%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean[] visited;
+    public boolean canReach(int[] arr, int start) {
+        
+        visited = new boolean[arr.length];
+        return dfs(arr, start);
+    }
+    
+    public boolean dfs(int[] arr, int idx){
+        
+        if(idx<0 || idx>=arr.length)
+            return false;
+        
+        if(visited[idx])
+            return false;
+        if(arr[idx]==0)
+            return true;
+        visited[idx] = true;
+        return (dfs(arr, idx+arr[idx]) || dfs(arr, idx-arr[idx]));
+        
+    }
+}
