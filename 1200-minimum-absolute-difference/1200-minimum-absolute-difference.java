91f9a7283fb5c31694761f4eb01545d4b4cb8a68
Charishma Boddu <charishma0249@gmail.com>
2022-02-16 00:33:19 -0700

    Time: 22 ms (20.62%), Space: 62.1 MB (5.09%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -2,15 +2,18 @@ class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         
         Arrays.sort(arr);
-        int min = Integer.MAX_VALUE;
+        int min = arr[arr.length-1];
         List<List<Integer>> res = new ArrayList<>();
+        int[] temp = new int[arr.length];
         
         for(int i=1; i<arr.length; i++){
-            min = Math.min(min, arr[i]-arr[i-1]);
+            temp[i-1] = arr[i]-arr[i-1];
+            min = Math.min(min, temp[i-1]);
+            
         }
         
         for(int i=1; i<arr.length; i++){
-            int diff = arr[i]-arr[i-1];
+            int diff = temp[i-1];
             
             if(min==diff){
                 List<Integer> pair = new ArrayList<>();
