42a744129c7e07ff14d904ec44b8481b018f0cfb
Charishma Boddu <charishma0249@gmail.com>
2022-02-16 22:18:34 -0700

    Time: 79 ms (5.48%), Space: 93.5 MB (11.88%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,26 +1,27 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        HashMap<Integer, HashSet<Integer>> hm = new HashMap<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
         
         for(int i=0; i<edges.length; i++){
             
-            HashSet<Integer> hsa = new HashSet<>();
-            HashSet<Integer> hsb = new HashSet<>();
+            int hsa=0, hsb=0;
+            // HashSet<Integer> hsa = new HashSet<>();
+            // HashSet<Integer> hsb = new HashSet<>();
             
             if(hm.containsKey(edges[i][0]))
                 hsa = hm.get(edges[i][0]);
             if(hm.containsKey(edges[i][1]))
                 hsb = hm.get(edges[i][1]);
             
-            hsa.add(edges[i][1]);
-            hsb.add(edges[i][0]);
+            // hsa.add(edges[i][1]);
+            // hsb.add(edges[i][0]);
             
-            hm.put(edges[i][0], hsa);
-            hm.put(edges[i][1], hsb);
+            hm.put(edges[i][0], hsa+1);
+            hm.put(edges[i][1], hsb+1);
         }
         
-        for(Map.Entry<Integer, HashSet<Integer>> entry : hm.entrySet()){
-            if(entry.getValue().size()==hm.size()-1)
+        for(Map.Entry<Integer, Integer> entry : hm.entrySet()){
+            if(entry.getValue()==hm.size()-1)
                 return entry.getKey();
         }
         return -1;
