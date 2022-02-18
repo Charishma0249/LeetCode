3511a7209c31335cd768cac89e02616786559717
Charishma Boddu <charishma0249@gmail.com>
2022-02-18 12:03:18 -0700

    Time: 5 ms (86.02%), Space: 43.9 MB (24.81%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -6,38 +6,23 @@ class Solution {
         
         if(len2<len1)
             return false;
-        for(char c:s1.toCharArray())
-            alp1[c-'a']++;
-        
         for(int i=0; i<len1; i++){
-            char c = s2.charAt(i);
-            alp2[c-'a']++;
+            char c1 = s1.charAt(i);
+            alp1[c1-'a']++;
+            char c2 = s2.charAt(i);
+            alp2[c2-'a']++;
         }
-        boolean flag=false;
-        for(int k=0; k<26; k++){
-                if(alp1[k]!=alp2[k]){
-                    flag =true;
-                    break;
-                }
-                    
-            }
-        if(!flag)
+        
+        if(Arrays.equals(alp1, alp2))
             return true;
+        
         for(int i=1, j=len1; j<len2; i++,j++){
             char ci = s2.charAt(i-1);
             alp2[ci-'a']--;
             char cj = s2.charAt(j);
             alp2[cj-'a']++;
             
-            flag=false;
-            for(int k=0; k<26; k++){
-                if(alp1[k]!=alp2[k]){
-                    flag =true;
-                    break;
-                }
-                    
-            }
-            if(!flag)
+            if(Arrays.equals(alp1, alp2))
                 return true;
         }
         
