239be2f12615109be8636bad4db36d6d38eba393
Charishma Boddu <charishma0249@gmail.com>
2022-02-18 23:09:54 -0700

    Time: 9 ms (30.07%), Space: 44.7 MB (34.21%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        Arrays.sort(candidates);
+        rec(candidates, target, 0, new ArrayList<Integer>(), 0);
+        return res;
+    }
+    
+    public void rec(int[] arr, int target, int sum, List<Integer> al, int j){
+        
+        if(sum==target){
+            res.add(new ArrayList<>(al));
+            return ;
+        }
+        if(sum>target)
+            return ;
+        
+        for(int i=j; i<arr.length; i++){
+            al.add(arr[i]);
+            rec(arr, target, sum+arr[i], al, i);
+            al.remove(al.size()-1);
+        }
+    }
+}
