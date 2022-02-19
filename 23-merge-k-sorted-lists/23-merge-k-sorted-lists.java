4d9bf61f8d038dc13ffb503ab9acc4beadc65043
Charishma Boddu <charishma0249@gmail.com>
2022-02-18 19:24:15 -0700

    Time: 9 ms (44.23%), Space: 47.6 MB (18.38%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -11,14 +11,16 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         
-        ListNode result = null;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<>(){
+        ListNode ls=null, res=ls;
+        
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>(){
             
             @Override
-            public int compare(ListNode p1, ListNode p2){
-                if(p1.val>p2.val)
+            public int compare(ListNode ls1, ListNode ls2){
+                
+                if(ls1.val>ls2.val)
                     return 1;
-                else if(p1.val<p2.val)
+                else if(ls1.val<ls2.val)
                     return -1;
                 return 0;
             }
@@ -28,22 +30,23 @@ class Solution {
             if(lists[i]!=null)
                 pq.add(lists[i]);
         }
-        boolean flag = false;
-        ListNode prev = result;
-        while(!pq.isEmpty()){
-            ListNode newn = new ListNode();
-            newn = pq.poll();
-            if(newn.next!=null)
-                pq.add(newn.next);
-            if(!flag){
-                flag=true;
-                result = newn;
-            }
-            else
-                prev.next = newn;
-            prev = newn;
-        }
         
-        return result;
+        while(!pq.isEmpty()){
+            ListNode node = pq.poll();
+            // System.out.println(node.val);
+            if(ls==null){
+                ls = new ListNode(node.val);
+                res=ls;
+            }
+            else{
+                ls.next = new ListNode(node.val);
+                ls = ls.next;
+            }
+            if(node.next!=null)
+                pq.add(node.next);
+            
+        }
+        // ls.next=null;
+        return res;
     }
 }
