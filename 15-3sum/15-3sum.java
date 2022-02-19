153e12ef4383a29e0d248c5cd881369567345d05
Charishma Boddu <charishma0249@gmail.com>
2022-02-18 17:18:58 -0700

    Time: 457 ms (21.40%), Space: 171 MB (5.00%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -6,10 +6,14 @@ class Solution {
         Arrays.sort(nums);
         
         for(int i=0; i<len; i++){
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
             int target = nums[i]*(-1);
             
             HashMap<Integer, Integer> hm = new HashMap<>();
             for(int j=i+1; j<len; j++){
+                // if(j-1>i+1 && nums[j]==nums[j-1])
+                //     continue;
                 
                 if(hm.containsKey(target-nums[j])){
                     List<Integer> al = new ArrayList<>();
@@ -17,8 +21,10 @@ class Solution {
                     al.add(target-nums[j]);
                     al.add(nums[j]);
                     // System.out.println(nums[i]+" "+nums[j] +" "+target);
-                    if(!res.contains(al))
+                    // if(!res.contains(al))
                         res.add(al);
+                    while(j+1<len && nums[j]==nums[j+1])
+                        j++;
                 }
                 else
                     hm.put(nums[j], j);
