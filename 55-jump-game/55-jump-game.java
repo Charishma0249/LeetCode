9351711f01bda9b482bb89e92e140255e4b16e13
Charishma Boddu <charishma0249@gmail.com>
2022-02-20 00:41:40 -0700

    Time: 642 ms (10.64%), Space: 70.5 MB (5.08%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,14 +1,28 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        int res = rec(nums, 0, dp);
         
-        int max = 0;
-        for(int i=0; i<nums.length; i++){
-            
-            if(i>max)
-                return false;
-            max = Math.max(max, nums[i]+i);
+        return (res==1)?true : false;
+    }
+    
+    public int rec(int[] n, int i, int[] dp){
+        
+        boolean res=false;
+        if(i==n.length-1)
+            return 1;
+        if(i>n.length || (i<n.length && n[i]==0))
+            return 0;
+        
+        if(dp[i]>=0)
+            return dp[i];
+        for(int j=1; j<=n[i]; j++){
+            dp[i] = rec(n, i+j, dp);
+            if(dp[i]==1)
+                return 1;
         }
         
-        return true;
+        return 0;
     }
 }
