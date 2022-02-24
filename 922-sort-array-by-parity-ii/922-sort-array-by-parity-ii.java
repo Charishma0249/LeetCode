d5c08b9b6ba289664f38d89386e1093bdea86ef4
Charishma Boddu <charishma0249@gmail.com>
2022-02-23 23:06:24 -0700

    Time: 8 ms (14.21%), Space: 55.5 MB (12.12%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
@@ -1,29 +1,27 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         int len = nums.length;
-        int k=-1, l=-1;
-        int[] res = new int[len];
         
         for(int i=0, j=0; i<len; i++){
             
             if(i%2==0 && nums[i]%2!=0){
-                j = i+1;
+                j=i+1;
                 while(j<len && nums[j]%2!=0)
-                    j++;
-                if(j<len && nums[j]%2==0){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+                    j = j+2;
+                if(j<len){
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
                 }
             }
             else if(i%2!=0 && nums[i]%2==0){
                 j = i+1;
                 while(j<len && nums[j]%2==0)
-                    j++;
-                if(j<len && nums[j]%2!=0){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+                    j = j+2;
+                if(j<len){
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
                 }
             }
         }
