f9085dcb651141e5e3c7a657dedeeca0e6d77c46
Charishma Boddu <charishma0249@gmail.com>
2022-02-23 22:58:10 -0700

    Time: 7 ms (17.11%), Space: 43.3 MB (85.52%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
@@ -1,23 +1,31 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         int len = nums.length;
-        int[] even = new int[len/2];
-        int[] odd = new int[len/2];
         int k=-1, l=-1;
+        int[] res = new int[len];
         
-        for(int i=0; i<len; i++){
-            if(nums[i]%2==0)
-                even[++k]=nums[i];
-            else
-                odd[++l]=nums[i];
-        }
-        
-        k=-1; l=-1;
-        for(int i=0; i<len; i++){
-            if(i%2==0)
-                nums[i]=even[++k];
-            else
-                nums[i]=odd[++l];
+        for(int i=0, j=0; i<len; i++){
+            
+            if(i%2==0 && nums[i]%2!=0){
+                j = i+1;
+                while(j<len && nums[j]%2!=0)
+                    j++;
+                if(j<len && nums[j]%2==0){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+            else if(i%2!=0 && nums[i]%2==0){
+                j = i+1;
+                while(j<len && nums[j]%2==0)
+                    j++;
+                if(j<len && nums[j]%2!=0){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
         }
         
         return nums;
