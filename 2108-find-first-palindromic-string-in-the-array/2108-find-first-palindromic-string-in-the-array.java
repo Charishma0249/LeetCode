4f0cb0681ee06233603aa7952c8cb74ae9d2441b
Charishma Boddu <charishma0249@gmail.com>
2022-02-23 23:09:45 -0700

    Time: 4 ms (59.78%), Space: 50.7 MB (33.41%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        
+        for(String s : words){
+            boolean res = isPalindrome(s);
+            if(res)
+                return s;
+        }
+        
+        return "";
+    }
+    
+    public boolean isPalindrome(String s){
+        
+        int len = s.length();
+        for(int i=0, j=len-1; i<j; i++, j--){
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(j);
+            
+            if(c1!=c2)
+                return false;
+        }
+        
+        return true;
+    }
+}
