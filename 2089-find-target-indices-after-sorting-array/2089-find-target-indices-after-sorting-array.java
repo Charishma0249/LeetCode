d8c9aa5f61afd56039909ca8be22350d92685296
Charishma Boddu <charishma0249@gmail.com>
2022-04-07 00:27:21 -0700

    Time: 2 ms (77.48%), Space: 42.7 MB (81.22%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,32 +1,14 @@
 class Solution {
-    
-    List<Integer> res = new ArrayList<>();
     public List<Integer> targetIndices(int[] nums, int target) {
         
-        int len = nums.length;
-        
+        List<Integer> al = new ArrayList<>();
         Arrays.sort(nums);
-        binSearch(nums, target, 0, len);
         
-        Collections.sort(res);
-        return res;
-    }
-    
-    public void binSearch(int[] nums, int target, int start, int end) {
-        
-        if(start>=end)
-            return ;
-        
-        int mid = (start+end)/2;
-        if(target<nums[mid])
-            binSearch(nums, target, start, mid);
-        else if(target>nums[mid])
-            binSearch(nums, target, mid+1, end);
-        else {
-            res.add(mid);
-            binSearch(nums, target, start, mid);
-            binSearch(nums, target, mid+1, end);
+        for(int i=0; i<nums.length; i++){
+            if(target == nums[i])
+                al.add(i);
         }
         
+        return al;
     }
 }
