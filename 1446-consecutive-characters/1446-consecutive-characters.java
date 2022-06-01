ddd68256f10d93e370210d36643b44c93261dd12
Charishma Boddu <charishma0249@gmail.com>
2022-05-31 22:20:41 -0400

    Time: 3 ms (25.66%), Space: 42.9 MB (21.49%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -5,6 +5,7 @@ class Solution {
         // alp[s.charAt(0)-'a']++;
         int temp = 1;
         int len = s.length();
+        int count = 0;
         
         for(int i=1; i<len; i++) {
             
@@ -16,16 +17,14 @@ class Solution {
             }
             else {
                 alp[c1] = Math.max(alp[c1], temp);
+                count = Math.max(count, temp);
                 temp=1;
             }
         }
         
         alp[s.charAt(len-1)-'a'] = Math.max(alp[s.charAt(len-1)-'a'], temp);
-        int count=0;
+        count = Math.max(count, temp);
         
-        for(int c : alp) {
-            count = Math.max(count, c);
-        }
         
         return count;
     }
