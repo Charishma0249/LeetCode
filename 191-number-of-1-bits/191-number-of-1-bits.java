da665d1e5bbe2965de1080894f1c8a53ae0187e1
Charishma Boddu <charishma0249@gmail.com>
2022-06-02 18:48:07 -0400

    Time: 2 ms (18.17%), Space: 40.9 MB (63.42%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -1,15 +1,10 @@
 public class Solution {
     // you need to treat n as an unsigned value
-    HashMap<Integer, Integer> hm = new HashMap<>();
     public int hammingWeight(int n) {
         int count = 0;
-        int num=0;
-        if(hm.containsKey(n))
-            return hm.get(n);
+        
         while(n!=0) {
-            int m = n&1;
-            count += m;
-            hm.put((int)(num+Math.pow(2, m)), count);
+            count += n&1;
             n = n>>>1;
         }
         
