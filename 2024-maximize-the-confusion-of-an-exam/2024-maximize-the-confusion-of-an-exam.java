1f9421654b8d232e69d210d8a6cfb2c5ce79842e
Charishma Boddu <charishma0249@gmail.com>
2022-06-02 16:59:12 -0400

    Time: 17 ms (67.63%), Space: 48 MB (52.33%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        
+        int len = answerKey.length();
+        int count=0, maxCount=0;
+        int trueCount =0, falseCount =0;
+        
+        for(int i=0, j=0; i<=j && j<len; j++) {
+            char c = answerKey.charAt(j);
+            
+            if(c=='T') {
+                trueCount++;
+            }
+            else {
+                falseCount++;
+            }
+            
+            count = Math.max(count, Math.max(trueCount, falseCount));
+            
+            if(j-i+1-count>k) {
+                while(j-i+1-count>k) {
+                    char c1 = answerKey.charAt(i);
+                    if(c1=='T') {
+                        trueCount--;
+                    }
+                    else {
+                        falseCount--;
+                    }
+                    i++;
+                }
+            }
+            
+            maxCount = Math.max(maxCount, j-i+1);
+        }
+        
+        return maxCount;
+    }
+}
