9c4b117417f6181a982c5934855fe55f5d43d550
Charishma Boddu <charishma0249@gmail.com>
2022-06-07 18:29:22 -0400

    Time: 1 ms (75.13%), Space: 41 MB (68.95%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -5,44 +5,20 @@ class Solution {
         ArrayList<ArrayList<Integer>> subsets = new ArrayList<>();
         
         // System.out.println("Hi - "+(6^1^1));
-        rec(nums, 0, new ArrayList<Integer>(), 0, subsets);
+        rec(nums, 0, 0);
         // System.out.println(subsets);
         return sum;
     }
     
-    public void rec(int[] nums, int j, ArrayList<Integer> arr, int val, ArrayList<ArrayList<Integer>> subsets) {
-        
+    public void rec(int[] nums, int j, int val) {
         int xor=0;
-        // if(j==nums.length-1) 
-        //     return nums[j];
-        // if(subsets.contains(new ArrayList<>(arr))) {
-        //     return ;
-        // }
-        // if(j<nums.length) {
-        //     val = val^nums[j];
-        //     sum += val;
-        //     // sum += nums[j];
-        //     // subsets.add(new ArrayList<>(arr));
-        //     System.out.println(val);
-        // }
-        // if(j==nums.length) {
-        //     return nums[j-1];
-        // }
+        
         for(int i=j; i<nums.length; i++) {
-            // arr.add(nums[i]);
             val = val^nums[i];
-             // System.out.println(val);
-            rec(nums, i+1, arr, val, subsets);
-            // rec(nums, i+1, arr, 0, subsets);
-            // if(!subsets.contains(new ArrayList<>(arr)))
-            //     subsets.add(new ArrayList<>(arr));
-            // subsets.add(new ArrayList<>(arr));
+            rec(nums, i+1, val);
             sum += val;
-            // arr.remove(arr.size()-1);
             val = val^nums[i];
             
         }
-        
-        // return xor;
     }
 }
