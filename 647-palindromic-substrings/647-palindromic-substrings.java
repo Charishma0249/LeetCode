65190d06e6ca600905b20083493e38165af580ed
Charishma Boddu <charishma0249@gmail.com>
2022-06-08 00:30:07 -0400

    Time: 31 ms (20.90%), Space: 51.2 MB (14.12%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSubstrings(String s) {
+        
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        int count = 0;
+        
+        for(int i=0; i<len; i++) {
+            dp[i][i] = 1;
+            count++;
+        }
+        
+        for(int k=1; k<len; k++) {
+            for(int i=0; i+k<len; i++) {
+                
+                char c1 = s.charAt(i);
+                char c2 = s.charAt(i+k);
+                if((c1==c2 && dp[i+1][i+k-1] == 1) || (c1==c2 && i+1==i+k)) {
+                    count++;
+                    dp[i][i+k] = 1;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
