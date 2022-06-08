eeba401ffddec153e3a443c0af21fc764086ac1c
Charishma Boddu <charishma0249@gmail.com>
2022-06-08 01:01:01 -0400

    Time: 14 ms (48.19%), Space: 45 MB (36.82%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -3,30 +3,32 @@ class Solution {
         
         ArrayList<Character> al = new ArrayList<>();
         
-        int n = x;
+        int n = x, m=0, k=0;
         if(x<0){
-            // al.add("-");
-            // n = x*(-1);
             return false;
         }
         
         while(n>0) {
             int r = n%10;
-            al.add(Character.valueOf((char)r));
+            // al.add(Character.valueOf((char)r));
+            // m = (m+r)*10;
+            // System.out.println(m);
+            // k++;
             n = n/10;
-        }
-        
-//         if(x<0) {
-//             al.add('-');
-//         }
-        
-        // System.out.println(al);
-        for(int i=0, j= al.size()-1; i<j; i++, j--) {
-            if(al.get(i)!=al.get(j)) {
-                return false;
+            if(n>0) {
+                m = (m+r)*10;
+            } else {
+                m = m+r;
             }
         }
+        // System.out.println(m);
+        return x==m;
+//         for(int i=0, j= al.size()-1; i<j; i++, j--) {
+//             if(al.get(i)!=al.get(j)) {
+//                 return false;
+//             }
+//         }
         
-        return true;
+//         return true;
     }
 }
