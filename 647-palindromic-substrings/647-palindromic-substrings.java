cc6939aba5394325fd774cc5b5824f3ef4594295
Charishma Boddu <charishma0249@gmail.com>
2022-06-08 00:32:25 -0400

    Time: 22 ms (29.31%), Space: 51.3 MB (13.77%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -5,16 +5,16 @@ class Solution {
         int[][] dp = new int[len][len];
         int count = 0;
         
-        for(int i=0; i<len; i++) {
-            dp[i][i] = 1;
-        }
+        // for(int i=0; i<len; i++) {
+        //     dp[i][i] = 1;
+        // }
         
         for(int k=1; k<len; k++) {
             for(int i=0; i+k<len; i++) {
                 
                 char c1 = s.charAt(i);
                 char c2 = s.charAt(i+k);
-                if((c1==c2 && dp[i+1][i+k-1] == 1) || (c1==c2 && i+1==i+k)) {
+                if(c1==c2 && ((dp[i+1][i+k-1] == 1) || (i+1==i+k) || (i+1==i+k-1))) {
                     count++;
                     dp[i][i+k] = 1;
                 }
