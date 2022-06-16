84d813386cc437842c2063c36f290d05e268df02
Charishma Boddu <charishma0249@gmail.com>
2022-06-16 16:24:20 -0400

    Time: 913 ms (8.12%), Space: 198.7 MB (6.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,33 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
         int len = s.length();
-        int[][] dp = new int[len][len];
-        int max = 0;
-        String result="";
+        int[][] dp = new int[len][len]; 
+        int maxLen = 0;
+        StringBuilder sb = new StringBuilder();
+        // String result;
         
-        for(int i=0; i<len; i++) {
-            dp[i][i] = 1;
-            result = s.substring(i, i+1);
-        }
-        
-        for(int k=1; k<len; k++) {
+        for(int k=0; k<len; k++) {
             for(int i=0; i+k<len; i++) {
-                
                 char c1 = s.charAt(i);
                 char c2 = s.charAt(i+k);
                 
-                if((c1==c2 && dp[i+1][i+k-1]==1) || (c1 == c2 && i+1==i+k)) {
-                    if(max<k+1) {
-                        max = k+1;
-                        result = s.substring(i, i+k+1);
-                    }
-                    dp[i][i+k] = 1;
+                if(c1==c2 && k<=1) {
+                    dp[i][i+k] = k+1;
+                }
+                else if(c1==c2 && dp[i+1][i+k-1]>0) {
+                    dp[i][i+k] = 2+dp[i+1][i+k-1];
                 } 
-                
+                if(maxLen<dp[i][i+k]) {
+                    maxLen = dp[i][i+k];
+                    sb = new StringBuilder(s.substring(i, i+k+1));
+                }
             }
         }
         
-        return result;
+        return sb.toString();
     }
 }
