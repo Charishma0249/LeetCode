84dc05a16a7d05ad7222fd48f8f76e5986054928
Charishma Boddu <charishma0249@gmail.com>
2022-06-18 21:10:08 -0400

    Time: 1 ms (77.66%), Space: 43.6 MB (50.86%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -14,35 +14,41 @@
  * }
  */
 class Solution {
-    int count = 0;
+    // int count = 0;
     public int minCameraCover(TreeNode root) {
         // if(root.left==null && root.right==null)
         //     return 1;
-        int val = dfs(root);
+        int count = dfs(root, root, 0);
         
-        if(val==0) {
-            // System.out.println(val);
-            count++;
-        }
         return count;
     }
     
-    public int dfs(TreeNode node) {
+    public int dfs(TreeNode node, TreeNode parent, int count) {
         
-        if(node==null)
-            return -1;
-        
-        int left = dfs(node.left);
-        int right = dfs(node.right);
-        
-        if ((left==0 || right==0)) {
-            node.val = 1;
-            count++;
-        } 
-        else if((left==1 || right==1) || (left==1 && right==2) || (left==2 && right==1)) {
-            node.val = 2;
+        if(node==null) {
+            return 0;
         }
         
-        return node.val;
+        int leftCount = dfs(node.left, node, count)-count;
+        int rightCount = dfs(node.right, node, count)-count;
+        int res = leftCount+rightCount+count;
+        
+        // if(node==parent) {
+        //     if(node.val==0)
+        //         return res+1;
+        //     return res;
+        // }
+        
+        if( (node.val==0) && parent.val!=1) {
+            parent.val = 1;
+            return res+1;
+        }
+        else if(node.val==1) {
+            if(parent.val==1) {
+                return res;
+            }
+            parent.val = 2;
+        }
+        return res;
      }
 }
