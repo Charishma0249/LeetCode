164c1c041183d41a30607f07f665323f29fbce63
Charishma Boddu <charishma0249@gmail.com>
2022-09-08 00:40:51 -0700

    Time: 3 ms (96.24%), Space: 41.6 MB (98.20%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -4,14 +4,14 @@ class Solution {
         int max = 0;
         for(String s:sentences) {
             
-            // String[] str = s.split("\\s+");
-            int len = 1;
-            for(int i=0; i<s.length(); i++) {
-                if(s.charAt(i)==' ') {
-                    len++;
-                }
-            }
-            max = Math.max(max, len);
+            String[] str = s.split(" ");
+            // int len = 1;
+            // for(int i=0; i<s.length(); i++) {
+            //     if(s.charAt(i)==' ') {
+            //         len++;
+            //     }
+            // }
+            max = Math.max(max, str.length);
         }
         
         return max;
