565842ceb0f646900db50171419d17c3b2ab8a7a
Charishma Boddu <charishma0249@gmail.com>
2022-10-28 00:13:33 -0700

    Time: 4 ms (37.02%), Space: 42 MB (53.36%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -2,7 +2,7 @@ class Solution {
     public String sortSentence(String s) {
         
         HashMap<Integer, String> hm = new HashMap<>();
-        
+        String[] arr = new String[10];
         String[] str = s.split("\\s+");
         
         StringBuilder result = new StringBuilder();
@@ -10,12 +10,15 @@ class Solution {
         for(String temp:str) {
             char c = temp.charAt(temp.length()-1);
             
-            hm.put(c-'0', temp.substring(0, temp.length()-1));
+            // hm.put(c-'0', temp.substring(0, temp.length()-1));
+            arr[c-'0'] = temp.substring(0, temp.length()-1);
         }
         
         for(int i=1; i<=str.length; i++) {
-            result.append(hm.get(i));
-            result.append(" ");
+            if(arr[i]!=null) {
+                result.append(arr[i]);
+                result.append(" ");
+            }
         }
         
         return result.substring(0, result.length()-1);
