74eff1761c13cfbcc19cc93011afb272144aa2b7
Charishma Boddu <charishma0249@gmail.com>
2022-10-28 00:24:42 -0700

    Time: 29 ms (6.82%), Space: 56.5 MB (65.16%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,13 +1,15 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        ArrayList<String> arrType = new ArrayList<>();
-        ArrayList<String> arrColor = new ArrayList<>();
-        ArrayList<String> arrName = new ArrayList<>();
+        
+        int size = items.size();
+        String[] arrType = new String[size];
+        String[] arrColor = new String[size];
+        String[] arrName = new String[size];
         
         for(int i=0; i<items.size(); i++) {
-            arrType.add(items.get(i).get(0));
-            arrColor.add(items.get(i).get(1));
-            arrName.add(items.get(i).get(2));
+            arrType[i] = items.get(i).get(0);
+            arrColor[i] = items.get(i).get(1);
+            arrName[i] = items.get(i).get(2);
         }
         if(ruleKey.equals("type")) {
             return countItems(arrType, ruleValue);
@@ -18,11 +20,11 @@ class Solution {
         }
     }
     
-    public int countItems(List<String> al, String ruleValue) {
+    public int countItems(String[] arr, String ruleValue) {
         int count = 0;
         
-        for(int i=0; i<al.size(); i++) {
-            if(al.get(i).equals(ruleValue)) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i].equals(ruleValue)) {
                 count++;
             }
         }
