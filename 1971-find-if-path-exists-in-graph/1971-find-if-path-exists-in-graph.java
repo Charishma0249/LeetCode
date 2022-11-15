4e9d4e21df40fa39a562c620a67d2c1503e185b9
Charishma Boddu <charishma0249@gmail.com>
2022-11-14 18:55:23 -0700

    Time: 263 ms (33.37%), Space: 234.8 MB (23.01%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,41 +1,45 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         
-        HashMap<Integer, List<Integer>> hm = new HashMap<>();
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        int num = edges.length;
+        Queue<Integer> qu = new LinkedList<>();
         HashSet<Integer> visited = new HashSet<>();
         
-        int len = edges.length;
-        for(int i=0; i<len; i++){
-            int node1 = edges[i][0];
-            int node2 = edges[i][1];
-            List<Integer> ls1 = new ArrayList<>();
-            List<Integer> ls2 = new ArrayList<>();
+        for(int i=0; i<num; i++) {
             
-            if(hm.containsKey(node1))
-                ls1 = hm.get(node1);
-            if(hm.containsKey(node2))
-                ls2 = hm.get(node2);
-                
-            ls1.add(node2);
-            ls2.add(node1);
+            ArrayList<Integer> arr = new ArrayList<>();
+            if(hm.containsKey(edges[i][0])) {
+                arr = hm.get(edges[i][0]);
+            }
             
-            hm.put(node1, ls1);
-            hm.put(node2, ls2);
+            arr.add(edges[i][1]);
+            hm.put(edges[i][0], arr);
+            
+            arr = new ArrayList<>();
+            if(hm.containsKey(edges[i][1])) {
+                arr = hm.get(edges[i][1]);
+            }
+            
+            arr.add(edges[i][0]);
+            hm.put(edges[i][1], arr);
         }
         
-        Queue<Integer> qu = new LinkedList<>();
-        
         qu.add(source);
-        while(!qu.isEmpty()){
-            int tempNode = qu.poll();
-            visited.add(tempNode);
-            if(tempNode==destination)
+        while(!qu.isEmpty()) {
+            int temp = qu.poll();
+            
+            if(visited.contains(temp))
+                continue;
+            
+            visited.add(temp);
+            if(temp==destination)
                 return true;
-            if(hm.containsKey(tempNode)){
-                List<Integer> ls = hm.get(tempNode);
-                for(int i=0; i<ls.size(); i++) {
-                    if(!visited.contains(ls.get(i)))
-                        qu.add(ls.get(i));
+            if(hm.containsKey(temp)) {
+                ArrayList<Integer> arr = hm.get(temp);
+                
+                for(int i=0; i<arr.size(); i++) {
+                    qu.add(arr.get(i));
                 }
             }
         }
@@ -43,10 +47,3 @@ class Solution {
         return false;
     }
 }
-
-// 0 -> 1,2
-// 1 -> 0
-// 2 -> 0
-// 3 -> 5,4
-// 5 -> 3,4
-// 4 -> 5,3
