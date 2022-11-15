9b769678c41606edf7d3068c6f45741d3ed1f958
Charishma Boddu <charishma0249@gmail.com>
2022-11-14 17:16:56 -0700

    Time: 56 ms (13.87%), Space: 93.7 MB (8.59%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,22 +1,35 @@
 class Solution {
     public int findCenter(int[][] edges) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int maxEdges = 0, centerNode=1;
         
-        int len = edges.length;
-        int[] arr = new int[len+1];
-        for(int i=0; i<len; i++){
+        for(int i=0; i<edges.length; i++) {
             
-            int node1 = edges[i][0];
-            int node2 = edges[i][1];
-            arr[node1-1]++;
-            arr[node2-1]++;
+            int count = 0;
+            if(hm.containsKey(edges[i][0])) {
+                count = hm.get(edges[i][0]);
+            } 
             
-            if(arr[node1-1]>1)
-                return node1;
-            else if(arr[node2-1]>1)
-                return node2;
+            hm.put(edges[i][0], count+1);
+            
+            if(maxEdges < count) {
+                maxEdges = count;
+                centerNode = edges[i][0];
+            }
+            count = 0;
+            
+            if(hm.containsKey(edges[i][1])) {
+                count = hm.get(edges[i][1]);
+            }
+            
+            hm.put(edges[i][1], count+1);
+            
+            if(maxEdges < count) {
+                maxEdges = count;
+                centerNode = edges[i][1];
+            }
         }
         
-        return 0;
+        return centerNode;
     }
 }
- 
